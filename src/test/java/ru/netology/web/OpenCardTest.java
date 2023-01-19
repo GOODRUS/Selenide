956236a98c941d0242959513d80671f33aed79bd
Dmitriy Shilyaev <goodrus1993@yandex.ru>
2023-01-19 16:43:14 +0500

    Initial commit

---
+++ src/test/java/ru/netology/web/OpenCardTest.java
@@ -0,0 +1,37 @@
+package ru.netology.web;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+class OpenCardTest {
+    @Test
+    void shouldRegisterByAccountNumberDOMModification() {
+
+        LocalDate date = LocalDate.now();
+        date = date.plusDays(3);
+        int year = date.getYear();
+        int month = date.getMonthValue();
+        int day = date.getDayOfMonth();
+        String dateFinal = day + "." + "0" + month + "." + year;
+
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999");
+        $("span[data-test-id='city'] input").setValue("Екатеринбург");
+        $("span[data-test-id='date'] input").sendKeys(Keys.CONTROL + "a" + Keys.BACK_SPACE);
+        $("span[data-test-id='date'] input").setValue(String.valueOf(dateFinal));
+        $("span[data-test-id='name'] input").setValue("Иванов Иван");
+        $("span[data-test-id='phone'] input").setValue("+79225788643");
+        $$("label[data-test-id='agreement']").find(exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных")).click();
+        $$("button").find(exactText("Забронировать")).click();
+        $x("//div[contains(text(), 'Успешно!')]").should(appear, Duration.ofSeconds(15));
+    }
+}
+
